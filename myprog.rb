47f93d6c94a65c7a9a08e1f63c10de3b2432b3f7
elliotec <elliotecweb@gmail.com>
2014-03-13 13:11:25 -0700

    up to reading and 'riting

--- myprog.rb
+++ myprog.rb
@@ -5,3 +5,22 @@ puts "The array is now #{a.inspect}"
 
 b = %w[ bee dog cat happy]
 puts "#{b}"
+
+def call_block
+  puts "Start of Method"
+  yield
+  yield
+  puts "End of Method"
+end
+call_block { puts "In the block" }
+
+def who_says_what
+  yield("Dave", "hello")
+  yield("Andy", "goodbye")
+end
+who_says_what { |person,phrase| puts "#{person} says #{phrase}" }
+
+[ 'cat', 'dog', 'horse' ].each {|name| print name, " " }
+5.times { print "*" }
+3.upto(6) { |i| print i }
+('a'..'e').each {|char| print char }
