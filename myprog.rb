7f73c5f64eb72883045a694882fc24a46cbdf108
elliotec <elliotecweb@gmail.com>
2014-03-13 00:44:51 -0700

    intro_1

--- myprog.rb
+++ myprog.rb
@@ -1,2 +1,10 @@
 puts "Hello, Ruby Programmer"
 puts "The time is #{Time.now}"
+
+def say_goodnight(name)
+  result = "Goodnight, " + name
+  return result
+end
+
+puts say_goodnight("John-Boy")
+puts say_goodnight("Mary-Ellen")
