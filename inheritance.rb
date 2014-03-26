1daa71d565fe8d8890f227178988fa785f449553
elliotec <elliotecweb@gmail.com>
2014-03-26 01:10:15 -0700

    inheritance

---
+++ inheritance.rb
@@ -0,0 +1,26 @@
+class Parent
+  def say_hello
+    puts "Hello from #{self}"
+  end
+end
+
+p = Parent.new
+p.say_hello
+
+class Child < Parent
+end
+
+c = Child.new
+c.say_hello
+
+puts "The superclass of Child is #{Child.superclass}"
+puts "The superclass of Parent is #{Parent.superclass}"
+puts "The superclass of Object is #{Object.superclass}"
+puts "The superclass of BasicObject is #{BasicObject.superclass.inspect}"
+
+class Person
+  def initialize(name)
+    @name = name
+  end
+end
+m = Person.new 
